7f5dc229323020498e0ebe5cfb4d8271381c0168
Ilia Tregub <volleyboler@gmail.com>
2025-07-10 17:27:29 +0500

    index update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,10 +12,3 @@ root.render(
      <App />
    </Provider>
 );
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// );
