76313bdeb97e4bf04a9f74bcc6f11b62c72509f6
Ilia Tregub <volleyboler@gmail.com>
2025-07-22 16:15:28 +0500

    settings url updated

--- backend/my_cloud/settings.py
+++ backend/my_cloud/settings.py
@@ -29,10 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
@@ -180,7 +179,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = os.getenv('ALLOWED_HOSTS').split(",")
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
 
 CORS_EXPOSE_HEADERS = [
   'Content-Type', 
